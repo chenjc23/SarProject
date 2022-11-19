b744d4ec090413f71d0624a2f0ee8ca6e8576667
chenjc23 <chenjc23@mail2.sysu.edu.cn>
2022-11-19 14:53:07 +0800

    feat: 提交新的baseAnalyse

--- toolBox/baseAnalyse.m
+++ toolBox/baseAnalyse.m
@@ -4,9 +4,33 @@
 % mul_times：扩展信号的插值倍数
 % win_length：样本窗长度，默认为64
 
-function baseAnalyse(sig, sample_freq, mul_times, win_length=64)
-  if 
+function baseAnalyse(sig, sample_freq, mul_times, win_length)
+  arguments 
+    sig (1,:)
+    sample_freq (1,1)
+    mul_times (1,1)
+    win_length (1,1) = 64
+  end
+  % 检查输入参数
+  if (length(sig) < win_length)
+    error('信号长度不能小于样本窗长度');
+  end
+
+  % 对信号进行窗截取
+  [mainPeak, mLoc] = max(abs(sig));
+  half = round(win_length/2);
+  sig_win = sig(mLoc-half:mLoc+half-1);
+
+  % 窗截取后扩展
+  S_win = fft(sig_win);
+  S_expend = [S_win(1:half), zeros(1, mul_times-1), S_win(half+1:end)];
+  sig_expend = fft(S_expend);
+
+  plot(sig_win);
+  hold on;
+  plot(sig_expend);
+
+
+
   
-
-
 end
